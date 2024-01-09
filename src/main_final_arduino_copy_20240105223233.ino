df96343ba58f414ffab97ab1ca05fccd2922a328
jordanangelo <124068677+jordanangelo@users.noreply.github.com>
2024-01-09 23:34:20 +0800

    Update main_final_arduino_copy_20240105223233.ino

--- src/main_final_arduino_copy_20240105223233.ino
+++ src/main_final_arduino_copy_20240105223233.ino
@@ -10,10 +10,13 @@
 // Arduino Rx=9, Tx=10
 SoftwareSerial nodemcu(9, 10);
 
-float distance;
+float distance1;
+float distance2;
 float duration1;
 float duration2;
 float level;
+float totalDist;
+
 long now = millis();
 long lastMeasure = 0;
 
@@ -25,41 +28,54 @@ void setup() {
   
   Serial.begin(115200);
   nodemcu.begin(9600);
-  delay(500);
+  delay(50);
   Serial.println("Program started");
 }
 
 void loop() {
+  now = millis();
   if (now - lastMeasure > 1000) {
     lastMeasure = now;
-    distanceFunc();
+    totalDist = 180 - (distanceFunc_1() + distanceFunc_2());
+    level = (totalDist / 180) * 100; // level in percentage
+
     StaticJsonBuffer<1000> jsonBuffer;
     JsonObject& data = jsonBuffer.createObject();
-
+    
     // assign data to a JSON object
     data["level"] = level;
 
     data.printTo(nodemcu);
+    Serial.print("Water Level: ");
+    Serial.print(level);
+    Serial.println("%");
+    
     jsonBuffer.clear();
   }
 }
 
-void distanceFunc() {
+float distanceFunc_1() {
   digitalWrite(trigPin1, LOW);
-  digitalWrite(trigPin2, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin1, HIGH);
-  digitalWrite(trigPin2, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin1, LOW);
-  digitalWrite(trigPin2, LOW);
 
   duration1 = pulseIn(echoPin1, HIGH);
+  distance1 = duration1 * 0.034 / 2;
+
+  return distance1;
+}
+
+float distanceFunc_2() {
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+
   duration2 = pulseIn(echoPin2, HIGH);
-  
-  distance = 180 - ((duration1 * 0.034 / 2) + (duration2 * 0.034 / 2));
-  level = (distance / 180) * 100; // level in percentage
-  Serial.print("Water Level: ");
-  Serial.print(level);
-  Serial.println("%");
+  distance2 = (duration2 * 0.034 / 2);
+
+  return distance2;
 }
